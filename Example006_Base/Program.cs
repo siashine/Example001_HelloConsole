177031b3825a3d8c965c9821f6f65bae36ed0737
Anastasia <kazantseva_nastya_95@mail.ru>
2022-10-01 23:46:22 +0500

    Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 2;
+int c = 8;
+int d = 2;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+
+Console.Write("max = ");
+Console.WriteLine(max);
